7e7bb13119b8bf1ff8ac0ab80632209cd3141381
Devbrat.Raghuvanshi <devbrat.raghuvanshi@ihsmarkit.com>
2017-05-31 16:15:27 +0530

    creating server using express and routing

--- app.js
+++ app.js
@@ -1,37 +1,25 @@
+var express = require("express");
 
-// #1 HTTP inbulit module is use to create an http server
-// 1. Require http module
-// 2. call create server function on http module
-// 3. call listen function to start a server
+var app = express();
 
-var http = require('http');
-
-// http.createServer(function (req, res) {
-//   res.writeHead(200, {'Content-Type': 'text/plain'});
-//   res.write('Hello World!');
-//   res.end();
-// }).listen(8080);
-
-var handleequest = function(req, res) {
-  if (req.url ==="/") {
-    res.write("root Dir");
-  }else if (req.url ==="/root1") {
-     res.write("root1 Dir");
-  } else {
-    res.write("root2 Dir");
-  }
-  res.end();
-}
-
-var server = http.createServer(handleequest);
-server.listen(8080,function() {
-    console.log("server is running on:");
-    console.log("http://localhost:8080/");
+app.listen(3000,function(){
+     console.log("server is running on:");
+    console.log("http://localhost:3000/");
     console.log("to open in browser Ctr + click on it");
-    
 });
 
 
+app.get("/",function(req,res) {
+    res.send("hello from Express");
+    res.end();
+})
 
-
+app.get("/books",function(req,res) {
+    res.send("hello from book page");
+    res.end();
+})
+app.get("/authors",function(req,res) {
+    res.send("hello from book authors");
+    res.end();
+})
 
